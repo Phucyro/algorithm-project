b3514d58d5a4ae9d3aa5d882d7c0df349f0d98e2
Jacana <jacana.robin@gmail.com>
2019-05-21 23:34:08 +0200

    Added class recuit_solver

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from nsga2 import nsga2
+from recuit_simule import recuit_solver
 
 if __name__ =="__main__":
-    mainSolver = nsga2(100, 30, 5)
-    mainSolver.execute()
+#    mainSolver = nsga2(100, 30, 5)
+#    mainSolver.execute()
+    recuitSolver = recuit_solver(150,0.3)
+    recuitSolver.solve()
--- recuit_simule.py
+++ recuit_simule.py
@@ -5,8 +5,8 @@ import city_parser
 from matplotlib import pyplot as plt
 from matplotlib import style
 
-NBRE_RES = 20 #Number of solutions that is going to be computed
-TIME_PER_SOL = 0.2 #Number of minutes allowed to optimize a solution
+NBRE_RES = 150 #Number of solutions that is going to be computed
+TIME_PER_SOL = 0.3 #Number of minutes allowed to optimize a solution
 
 class recuit(Annealer): #We use simanneal that implement the Annealer Simulated algorithm
 
@@ -50,60 +50,61 @@ def soltions_to_csv(solutions,name):
             output.write("0;\n")
             output.write(money+"\n")
 
+class recuit_solver():
+    def __init__(self,nbre_sol,time_per_sol):
+        self.NBRE_RES = nbre_sol #Number of solutions that is going to be computed
+        self.TIME_PER_SOL = time_per_sol #Number of minutes allowed to optimize a solution
+
+    def solve(self):
+        init_state = chromosome.tour()
+        rec = recuit(init_state)
+        auto_schedule = rec.auto(minutes=self.TIME_PER_SOL) #Get the parameters so that every solutions in computed in TIME_PER_SOL * minutes
+
+        to_show = []
+        #This loop compute the values with the annealar simulated algorithm
+        for i in range(0,self.NBRE_RES):
+            print(str(i)+"/"+str(self.NBRE_RES)+"\n")
+            init_state = chromosome.tour() #Create a random solution
+            rec = recuit(init_state) #Create the Annealer simulation
+            rec.set_schedule(auto_schedule) #Set the max / min temperature and the nbre of steps
+            it,miles = rec.anneal() #Run the simulation
+            to_show.append(it)
+
+        pareto,other = get_pareto(to_show) #Compute the pareto optimum values
+
+        # Get X and Y values for pareto optimum solutions
+
+        risk_p = []
+        dist_p = []
+        for j in pareto:
+            risk_p.append(j.mean_risk)
+            dist_p.append(j.tot_dist)
 
 
-init_state = chromosome.tour()
-rec = recuit(init_state)
-auto_schedule = rec.auto(minutes=TIME_PER_SOL) #Get the parameters so that every solutions in computed in TIME_PER_SOL * minutes
+        # Get X and Y values for non pareto optimum solutions
+        risk = []
+        dist = []
+        for j in other:
+            risk.append(j.mean_risk)
+            dist.append(j.tot_dist)
 
 
-to_show = []
-#This loop compute the values with the annealar simulated algorithm
-for i in range(0,NBRE_RES):
-    print(str(i)+"/"+str(NBRE_RES)+"\n")
-    init_state = chromosome.tour() #Create a random solution
-    rec = recuit(init_state) #Create the Annealer simulation
-    rec.set_schedule(auto_schedule) #Set the max / min temperature and the nbre of steps
-    it,miles = rec.anneal() #Run the simulation
-    to_show.append(it)
+        text_file = open(str(NBRE_RES)+"_"+str(TIME_PER_SOL)+".txt", "w")
+        for i in pareto:
+            text_file.write(str(i)+"\n")
 
+        text_file.close()
 
+        soltions_to_csv(pareto, "AnnealerSimulated2")
 
-pareto,other = get_pareto(to_show) #Compute the pareto optimum values
-
-# Get X and Y values for pareto optimum solutions
-
-risk_p = []
-dist_p = []
-for j in pareto:
-    risk_p.append(j.mean_risk)
-    dist_p.append(j.tot_dist)
-
-
-# Get X and Y values for non pareto optimum solutions
-risk = []
-dist = []
-for j in other:
-    risk.append(j.mean_risk)
-    dist.append(j.tot_dist)
-
-
-text_file = open(str(NBRE_RES)+"_"+str(TIME_PER_SOL)+".txt", "w")
-for i in pareto:
-    text_file.write(str(i)+"\n")
-
-text_file.close()
-
-soltions_to_csv(pareto, "AnnealerSimulated")
-
-#Make a graph with the soltions
-style.use('ggplot')
-f, ax = plt.subplots(1)
-plt.scatter(risk_p,dist_p,c="blue") # Pareto optimum colored in blue
-plt.scatter(risk,dist,c="red") # Other colored in red
-plt.title('Annealar')
-plt.ylabel('Total Distance (m)')
-plt.xlabel('Risk')
-plt.autoscale(enable = True,axis='both')
-plt.savefig(str(NBRE_RES)+"_"+str(TIME_PER_SOL)+".png")
-plt.show()
+        #Make a graph with the soltions
+        style.use('ggplot')
+        f, ax = plt.subplots(1)
+        plt.scatter(risk_p,dist_p,c="blue") # Pareto optimum colored in blue
+        plt.scatter(risk,dist,c="red") # Other colored in red
+        plt.title('Annealar')
+        plt.ylabel('Total Distance (m)')
+        plt.xlabel('Risk')
+        plt.autoscale(enable = True,axis='both')
+        plt.savefig(str(NBRE_RES)+"_"+str(TIME_PER_SOL)+".png")
+        plt.show()
