42953820ffcce5e5af474ea38254984fd7868348
antandrieux <antoine_andrieux@hotmail.com>
2019-05-21 13:02:12 +0200

    no duplicate no same random

--- nsga2.py
+++ nsga2.py
@@ -2,10 +2,6 @@ import chromosome
 import random
 import copy
 
-import numpy as np
-import random as rn
-
-
 class nsga2():
 
     def __init__(self, nombrePop):
@@ -168,8 +164,26 @@ class nsga2():
     def choosePopulation(self):
 
         self.population.clear()
-        print(self.front)
-        for rank in self.front:
+        randomList = {}
+        lenFront = len(self.front) - 1
+        if lenFront > 0:
+            while len(self.population) < self.popSize:
+                i = random.randint(0, lenFront)
+                lenSol = len(self.front[i]) - 1
+                if lenSol > 0:
+                    j = random.randint(0, lenSol)
+                    if not self.findDuplicate(self.front[i][j], self.population):
+                        if i in randomList:
+                            if j in randomList[i]:
+                                continue
+                            else:
+                                randomList[i].append(j)
+                                self.population.append(self.front[i][j])
+                        else:
+                            randomList[i] = [j]
+                            self.population.append(self.front[i][j])
+        """for rank in self.front:
+
             for solution in rank:
 
                 if len(self.population) < self.popSize:
@@ -179,7 +193,7 @@ class nsga2():
                     break
 
             if len(self.population) >= self.popSize:
-                break
+                break"""
 
     def showResultPareto(self):
 
@@ -197,120 +211,20 @@ class nsga2():
                 tot_dist, tot_risk = solution.get_total_dist()
                 print("Solution avec tot dist {} et tot risk {}".format(tot_dist, tot_risk))
 
-    def calculate_crowding(self, scores):
-        # Crowding is based on a vector for each individual
-        # All dimension is normalised between low and high. For any one dimension, all
-        # solutions are sorted in order low to high. Crowding for chromsome x
-        # for that score is the difference between the next highest and next
-        # lowest score. Total crowding value sums all crowding for all scores
-
-        population_size = len(scores[0])
-        number_of_scores = len(scores[:])
-
-        # create crowding matrix of population (row) and score (column)
-        crowding_matrix = np.zeros((population_size, number_of_scores))
-
-        # normalise scores (ptp is max-min)
-        normed_scores = (scores - scores.min(0)) / scores.ptp(0)
-
-        # calculate crowding distance for each score in turn
-        for col in range(number_of_scores):
-            crowding = np.zeros(population_size)
-
-            # end points have maximum crowding
-            crowding[0] = 1
-            crowding[population_size - 1] = 1
-
-            # Sort each score (to calculate crowding between adjacent scores)
-            sorted_scores = sorted(normed_scores[:, col], key=lambda x: [x.tot_dist, x.mean_risk], reverse=True)
-
-            sorted_scores_index = np.argsort(
-                normed_scores[:, col])
-
-            # Calculate crowding distance for each individual
-            crowding[1:population_size - 1] = \
-                (sorted_scores[2:population_size] -
-                sorted_scores[0:population_size - 2])
-
-            # resort to orginal order (two steps)
-            re_sort_order = np.argsort(sorted_scores_index)
-            sorted_crowding = crowding[re_sort_order]
-
-            # Record crowding distances
-            crowding_matrix[:, col] = sorted_crowding
-
-        # Sum crowding distances of each score
-        crowding_distances = np.sum(crowding_matrix, axis=1)
-
-        return crowding_distances
-
-    def reduce_by_crowding(self, scores, number_to_select):
-        # This function selects a number of solutions based on tournament of
-        # crowding distances. Two members of the population are picked at
-        # random. The one with the higher croding dostance is always picked
-        scores = np.array(self.front)
-        number_to_select = self.popSize - 1
-        population_ids = np.arange(scores.shape[0])
-
-        crowding_distances = self.calculate_crowding(scores)
-
-        picked_population_ids = np.zeros((number_to_select))
-
-        picked_scores = np.zeros((number_to_select, len(scores[0, :])))
-
-        for i in range(number_to_select):
-
-            population_size = population_ids.shape[0]
-
-            fighter1ID = rn.randint(0, population_size - 1)
-
-            fighter2ID = rn.randint(0, population_size - 1)
-
-            # If fighter # 1 is better
-            if crowding_distances[fighter1ID] >= crowding_distances[
-                fighter2ID]:
-
-                # add solution to picked solutions array
-                picked_population_ids[i] = population_ids[
-                    fighter1ID]
-
-                # Add score to picked scores array
-                picked_scores[i, :] = scores[fighter1ID, :]
-
-                # remove selected solution from available solutions
-                population_ids = np.delete(population_ids, (fighter1ID), axis=0)
-
-                scores = np.delete(scores, (fighter1ID), axis=0)
-
-                crowding_distances = np.delete(crowding_distances, (fighter1ID), axis=0)
-            else:
-                picked_population_ids[i] = population_ids[fighter2ID]
-
-                picked_scores[i, :] = scores[fighter2ID, :]
-
-                population_ids = np.delete(population_ids, (fighter2ID), axis=0)
-
-                scores = np.delete(scores, (fighter2ID), axis=0)
-
-                crowding_distances = np.delete(
-                    crowding_distances, (fighter2ID), axis=0)
-                
-        # Convert to integer
-        picked_population_ids = np.asarray(picked_population_ids, dtype=int)
-        return (picked_population_ids)
-
 test = nsga2(30)
 test.init_population(100)
 
 i=0
 
-while (i<10):
+while (i<3):
 
     test.createOffsprings()
     test.combineAndSort()
-    test.reduce_by_crowding(0,0)
+    test.choosePopulation()
 
     i+=1
+
+test.showResultPareto()
 
 
 
